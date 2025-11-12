af4b81e38fa38707eecc4b74610c470aeca7e2de
alumno <alumno@Instituto.local>
2025-11-12 08:20:42 -0300

    12/11

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC7NNnbjbrx4fFBfzaxkm-Klu8dXefiogQ",
-  authDomain: "huerta2025-e3f73.firebaseapp.com",
-  projectId: "huerta2025-e3f73",
-  storageBucket: "huerta2025-e3f73.firebasestorage.app",
-  messagingSenderId: "1048957792635",
-  appId: "1:1048957792635:web:27d7a44e1b96b4018c4dde",
-  measurementId: "G-9D1D0MQ355"
+    apiKey: "AIzaSyCrB4xgCBRmpujDcvOOihSVZ6MIFOPE56M",
+    authDomain: "proyectoclase1-2025.firebaseapp.com",
+    databaseURL: "https://proyectoclase1-2025-default-rtdb.firebaseio.com",
+    projectId: "proyectoclase1-2025",
+    storageBucket: "proyectoclase1-2025.firebasestorage.app",
+    messagingSenderId: "822869341917",
+    appId: "1:822869341917:web:8d4d76a6493df78963cd34"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+let parrafo = document.querySelector("p");
+
+const refDatos = ref(db, "huerta");
+
+onValue(refDatos, (snapshot) => {
+    console.log(snapshot.val())
+    let huerta = snapshot.val()
+    parrafo.textContent = `La huerta tiene una temperatura en el suelo de ${huerta.tempSuelo}° y de humedad de ${huerta.humSuelo}`
+    
+   
+})

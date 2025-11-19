dacfd52f64b389fb301d7f82e9fdef29b914005f
ValentinCangas <136343974+ValentinCangas@users.noreply.github.com>
2025-11-19 08:28:40 -0300

    Create aire.js

---
+++ aire.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCrB4xgCBRmpujDcvOOihSVZ6MIFOPE56M",
+    authDomain: "proyectoclase1-2025.firebaseapp.com",
+    databaseURL: "https://huertamaxi2025-default-rtdb.firebaseio.com",
+    projectId: "proyectoclase1-2025",
+    storageBucket: "proyectoclase1-2025.firebasestorage.app",
+    messagingSenderId: "822869341917",
+    appId: "1:822869341917:web:8d4d76a6493df78963cd34"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+const refDatos = ref(db, '/');
+let parrafo = document.querySelector("h1");
+
+
+
+onValue(refDatos, (snapshot) => {
+    console.log(snapshot.val())
+    let huerta = snapshot.val()
+    console.log()
+    parrafo.textContent = `La humedad del aire es de un ${huerta.humedadAire}%`
+    
+   
+})
